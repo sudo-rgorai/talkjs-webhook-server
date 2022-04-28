79a16191f582c69d3cf430f9cd7e0d80921d8724
sudo-rgorai <rahul.gorai.3141@gmail.com>
2022-04-28 17:45:33 +0530

    Update port

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ app.use(bodyParser.urlencoded({
 })); 
 app.use(cors());
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('Server is up'));
+app.listen(PORT, () => console.log('Server is up'));
 app.get("/", (req, res) => {
     res.status(200).end('TalkJS Event Monitor');
 })
